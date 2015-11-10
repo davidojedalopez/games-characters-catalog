656801345c99d8ecd8fe857046d09d2b76ccf7ac
David Ojeda <david.ojeda.lopez@gmail.com>
2015-11-09 20:46:33 -0600

    added all characters page

--- webserver.py
+++ webserver.py
@@ -17,12 +17,18 @@ def showGames():
 	games = session.query(Game)
 	return render_template("games.html", games=games)
 
+@app.route("/characters/")
+def showAllCharacters():
+	characters = session.query(Character).all()
+	return render_template("characters.html", characters=characters)
+
 @app.route("/games/<game_name>/")
 @app.route("/games/<game_name>/characters")
 def showCharacters(game_name):
 	game = session.query(Game).filter_by(name=game_name).one()
 	characters = session.query(Character).filter_by(game_id=game.id).all()
 	return render_template("characters.html", characters=characters, game=game)
+
 
 if __name__ == "__main__":
 	app.debug = True
