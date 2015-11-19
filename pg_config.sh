1ebe5c65c5e7c92caa770c57b143246a11168c16
David Ojeda <david.ojeda.lopez@gmail.com>
2015-11-19 09:48:05 -0600

    updated requirements for vagrant provisioning

--- pg_config.sh
+++ pg_config.sh
@@ -2,9 +2,10 @@ apt-get -qqy update
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
-pip install werkzeug==0.8.3
-pip install flask==0.9
-pip install Flask-Login==0.1.3
+pip install werkzeug==0.11.2
+pip install Flask==0.10.1
+pip install Flask-Login==0.3.2
+pip install rauth==0.7.2
 pip install oauth2client
 pip install requests
 pip install httplib2
